48b326d1d131f013551188b6db9ab954933c7a16
siyuan808 <hanyahui808@gmail.com>
2015-10-30 13:45:34 -0500

    Create CombinationSum.cc

---
+++ CombinationSum.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <unordered_set>
+#include <limits.h>
+
+using namespace std;
+
+void dfs(vector<int>& nums, int target, vector<int>& sol, vector<vector<int>>& res) {
+    if(target == 0) {
+        res.push_back(sol);
+        return;
+    }
+    if(target < 0) return;
+    
+    for(int n : nums) {
+        sol.push_back(n);
+        dfs(nums, target-n, sol, res);
+        sol.pop_back();
+    }
+}
+
+vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+    vector<vector<int>> res;
+    vector<int> sol;
+    dfs(nums, target, sol, res);
+    return res;
+}
+
+
+int main() {
+    vector<int> nums{4,3,1};
+    vector<vector<int>> combinations = combinationSum(nums, 5);
+    for(vector<int> comb : combinations ) {
+        for(int x : comb) {
+            cout <<x<<" ";
+        }
+        cout <<endl;
+    }
+    return 0;
+}
